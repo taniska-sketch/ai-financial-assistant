cad88225c026ec34b1e4852793766dbec0d79941
Taniska Saxena <taniskasaxena06@gmail.com>
2026-03-26 23:41:28 +0530

    fix model loading issue

--- backend/utils.py
+++ backend/utils.py
@@ -2,20 +2,8 @@ import joblib
 import re
 import os
 
-# Get current file path
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Go to models folder
 MODEL_PATH = os.path.join(BASE_DIR, "..", "models")
-
-# Load models
-cat_model = joblib.load(os.path.join(MODEL_PATH, "categorization_model.pkl"))
-vectorizer = joblib.load(os.path.join(MODEL_PATH, "vectorizer.pkl"))
-cash_model = joblib.load(os.path.join(MODEL_PATH, "cashflow_model.pkl"))
-anomaly_stats = joblib.load(os.path.join(MODEL_PATH, "anomaly_stats.pkl"))
-
-mean = anomaly_stats['mean']
-std = anomaly_stats['std']
 
 
 def clean_text(text):
@@ -25,15 +13,24 @@ def clean_text(text):
 
 
 def predict_category(text):
+    cat_model = joblib.load(os.path.join(MODEL_PATH, "categorization_model.pkl"))
+    vectorizer = joblib.load(os.path.join(MODEL_PATH, "vectorizer.pkl"))
+
     text = clean_text(text)
     vec = vectorizer.transform([text])
     return cat_model.predict(vec)[0]
 
 
 def predict_cashflow(month, day, weekday):
+    cash_model = joblib.load(os.path.join(MODEL_PATH, "cashflow_model.pkl"))
     return float(cash_model.predict([[month, day, weekday]])[0])
 
 
 def detect_anomaly(amount):
+    anomaly_stats = joblib.load(os.path.join(MODEL_PATH, "anomaly_stats.pkl"))
+
+    mean = anomaly_stats['mean']
+    std = anomaly_stats['std']
+
     z = (amount - mean) / std
     return 1 if abs(z) > 3 else 0
